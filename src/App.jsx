531f60ffebd2e42f676485b5e51051dd48b8b1f7
githubnali <nagarajunnr341@gmail.com>
2026-08-08 00:47:34 +0530

    [initial setup]

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='text-3xl font-bold underline text-center m-5 p-5'>Let's Build NetflixGPT from Scratch</h1>
+    </>
+  )
+}
+
+export default App
